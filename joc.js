bef1698856f96892f464c7dbaace1bd6ba2cfd03
Adrián Cárdenas Lara <32141101+SonHak@users.noreply.github.com>
2017-10-17 23:17:15 +0200

    comentado

--- joc.js
+++ joc.js
@@ -1,23 +1,29 @@
 function flip(event){
 	var clase = event.currentTarget.className;
-	var carta1 = "";
-	var carta2 = "";
+//	var carta1 = "";
+//	var carta2 = "";
 	
-	if(carta1 == ""){
-		carta1 = event.currentTarget;
+//	if(carta1 == ""){
+//		carta1 = event.currentTarget;
+//		event.currentTarget.setAttribute('class','flip');
+//	}
+//	else if(carta1 != "" && carta2 == ""){
+//		carta2 = event.currentTarget;
+//		event.currentTarget.setAttribute('class','flip');
+//	}else if(carta1 != carta2){
+//		carta1.setAttribue('class','flip-container');
+//		carta2.setAttribue('class','flip-container');
+//	}
+	
+	
+	
+	
+	if(clase != 'flip-container'){
+		event.currentTarget.setAttribute('class','flip-container');
+	}else{
 		event.currentTarget.setAttribute('class','flip');
 	}
-	else if(carta1 != "" && carta2 == ""){
-		carta2 = event.currentTarget;
-		event.currentTarget.setAttribute('class','flip');
-	}else if(carta1 != carta2){
-		carta1.setAttribue('class','flip-container');
-		carta2.setAttribue('class','flip-container');
-	}
 	
-	
-	
-
 	
 }
 
