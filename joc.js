d5fb9d52fb21e2d8e0f17bf76deca9eb65494681
Adrian Cardenas <SonHak@github.com>
2017-10-20 18:36:50 +0200

    subir

--- joc.js
+++ joc.js
@@ -1,29 +1,26 @@
 function flip(event){
 	var clase = event.currentTarget.className;
+	
 	var carta1 = "";
 	var carta2 = "";
 	
-//	if(carta1 == ""){
-//		carta1 = clase;
-//		event.currentTarget.setAttribute('class','flip');
-//	}
-//	else if(carta1 != "" && carta2 == ""){
-//		carta2 = clase;
-//		event.currentTarget.setAttribute('class','flip');
-//		
-//	}else if(carta1 != carta2){
-//		carta1.setAttribue('class','flip-container');
-//		carta2.setAttribue('class','flip-container');
-//	}
 	
-	
-	
-	
-	if(clase != 'flip-container'){
-		event.currentTarget.setAttribute('class','flip-container');
-	}else{
+	if(carta1 == ""){
+		carta1 = event.currentTarget.className;
+		event.currentTarget.setAttribute('class','flip');
+	}else if(carta1 != "" && carta2 == ""){
+		carta2 = event.currentTarget.className;
 		event.currentTarget.setAttribute('class','flip');
 	}
+	
+	document.getElementById("prueba").innerHTML = carta1;
+	
+	
+//	if(clase != 'flip-container'){
+//		event.currentTarget.setAttribute('class','flip-container');
+//	}else{
+//		event.currentTarget.setAttribute('class','flip');
+//	}
 	
 	
 }
